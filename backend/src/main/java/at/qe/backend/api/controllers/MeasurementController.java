87234e0dbdebc44d169144e504c0c4c68e3c7653
pimpmfl <danielpopescu934@gmail.com>
2023-03-13 23:00:27 +0100

    added multiple CORS origins

--- backend/src/main/java/at/qe/backend/api/controllers/MeasurementController.java
+++ backend/src/main/java/at/qe/backend/api/controllers/MeasurementController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
 @RequestMapping("/api")
 public class MeasurementController {
     @Autowired
--- backend/src/main/java/at/qe/backend/controllers/EmployeeController.java
+++ backend/src/main/java/at/qe/backend/controllers/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
 public class EmployeeController {
     @GetMapping("/test")
     public String fetchEmployees(){
