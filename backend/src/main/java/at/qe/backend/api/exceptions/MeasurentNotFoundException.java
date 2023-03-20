5f62eea4bca6284fafb65c8d0ac4c1b1a7249905
Tobias Beiser (Laptop) <tobias.beiser@gmail.com>
2023-03-20 16:43:34 +0100

    changed security context so only authorized users can access the api

---
+++ backend/src/main/java/at/qe/backend/api/exceptions/MeasurentNotFoundException.java
@@ -0,0 +1,11 @@
+package at.qe.backend.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code= HttpStatus.NOT_FOUND, reason = "The measurement with the given ID does not exists")
+public class MeasurentNotFoundException  extends Exception{
+    public MeasurentNotFoundException(){
+
+    }
+}
