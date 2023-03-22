79dea898ab8621c7f4c42f0b38fc28aa8ee04d8d
doubleg <georg.gas@live.at>
2023-03-22 17:45:09 +0100

    added header view

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,11 @@
 import test from '../views/test.vue'
 import login from '../views/Login.vue'
+import header from '../views/header.vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
 const routes = [
     { path: '/', component: test},
-    { path: '/login', component: login}
+    { path: '/login', component: login},
+    { path: '/header', component: header}
 ]
 
 export const router = createRouter({
