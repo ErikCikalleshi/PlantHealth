7afa8ff993193b9bca7e456ed21e518749c63d5d
Georg Gasser <csaz8052@student.uibk.ac.at>
2023-03-23 17:00:01 +0100

    footer in progress

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,13 @@
 import test from '../views/test.vue'
 import login from '../views/Login.vue'
 import header from '../views/header.vue'
+import footer from '../views/footer.vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
 const routes = [
     { path: '/', component: test},
     { path: '/login', component: login},
-    { path: '/header', component: header}
+    { path: '/header', component: header},
+    { path: '/footer', component: footer}
 ]
 
 export const router = createRouter({
---
+++ frontend/src/views/footer.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+import { OhVueIcon } from "oh-vue-icons";
+
+export default defineComponent({
+  name: "FooterView",
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    OhVueIcon
+  },
+})
+</script>
+
+<template>
+  <v-app>
+    <div class="footer-view pb-[0px]">
+      <div class="pl-[174px] pb-[207px]">
+        <img alt="Plant Health Gardening LOGO" src="@/assets/logo.svg"/>
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<style>
+.footer-view {
+  background-color: #040A0F;;
+  background-size: 1920px 323px;
+}
+html { overflow-y: auto !important; }
+
+</style>
