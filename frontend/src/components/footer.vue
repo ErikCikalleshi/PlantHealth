4324e7021f14894866458579364ab16dc293c337
doubleg <georg.gas@live.at>
2023-03-27 17:40:07 +0200

    fixed footer background

--- frontend/src/components/footer.vue
+++ frontend/src/components/footer.vue
@@ -9,33 +9,33 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="footer-view display-flex h-[30vh] w-[100vw]">
+  <div class="footer-view display-flex w-[100vw]">
     <div class="wrapper space-x-40 flex">
       <div class="logo h-full flex-1">
         <img alt="Plant Health Gardening LOGO" src="@/assets/logo.svg"/>
       </div>
       <div class="text-white flex-1">
-        <p class="font-secondary text-[20px] mb-3">Documentation:</p>
+        <p class="font-primary text-[20px] mb-3">Documentation:</p>
         <p class="font-secondary text-[16px]">- All categories</p>
         <p class="font-secondary text-[16px]">- Product information portal</p>
         <p class="font-secondary text-[16px]">- Datasheets</p>
       </div>
       <div class="text-white flex-1">
-        <p class="font-secondary text-[20px] mb-3">Company:</p>
+        <p class="font-primary text-[20px] mb-3">Company:</p>
         <p class="font-secondary text-[16px]">- Terms of Services</p>
         <p class="font-secondary text-[16px]">- Privacy Policy</p>
         <p class="font-secondary text-[16px]">- GDPR Protection</p>
         <p class="font-secondary text-[16px]">- Cookies Policy</p>
       </div>
       <div class="text-white flex-1">
-        <p class="font-secondary text-[20px] mb-3">Services:</p>
+        <p class="font-primary text-[20px] mb-3">Services:</p>
         <p class="font-secondary text-[16px]">- Products</p>
         <p class="font-secondary text-[16px]">- Software</p>
         <p class="font-secondary text-[16px]">- Hardware</p>
       </div>
     </div>
-    <div>
-      <p class="text-center font-secondary text-[16px] text-white opacity-60">Copyright 2022. PlantHealth. All rights reserved.</p>
+    <div class="flex-1 flex-row">
+      <p class="text-center font-secondary text-[16px] text-white opacity-60 ">Copyright ⓒ 2023. PlantHealth. All rights reserved.</p>
     </div>
   </div>
 </template>
@@ -43,6 +43,8 @@ export default defineComponent({
 <style scoped>
 .footer-view {
   background-color: #040A0F;
+  background-size: cover;
+  background-position: center center;
 }
 
 .logo img {
