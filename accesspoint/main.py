aecf2557b1a759b13a6480e59d2e6fdac444aee6
Th3RapidK1ller <erik.cikalleshi@student.uibk.ac.at>
2023-03-29 21:41:13 +0200

    get_config request

--- accesspoint/main.py
+++ accesspoint/main.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
-    print("Hello World")
+import requests
+
+url = "http://localhost:9000/api/setting/1"
+
+# basic auth
+admin = "admin"
+password = "passwd"
+auth = (admin, password)
+
+# make the request
+response = requests.get(url, auth=auth)
+
+# check the response status code
+if response.status_code == 200:
+    # process the response data
+    data = response.json()
+    print(data)
+else:
+    print("Error: API call failed with status code", response.status_code)
