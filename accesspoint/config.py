fe1baa1e56a835cdb5cf021f288cce0f94da1668
Th3RapidK1ller <erik.cikalleshi@student.uibk.ac.at>
2023-03-29 21:55:43 +0200

    get_config request cleaned

---
+++ accesspoint/config.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+def get_config(ip: str, port: str, admin: str, password: str):
+    url = f"http://{ip}:{port}/api/setting/1"
+    auth = (admin, password)
+    try:
+        response = requests.get(url, auth=auth)
+    except requests.exceptions.ConnectionError:
+        print("Error: 'api/setting/' API call failed")
+        return response.status_code
+
+    data = response.json()
+    with open("config/config.json", "w") as f:
+        json.dump(data, f)
+    return response.status_code
+
+
+# for debug purposes only
+if __name__ == "__main__":
+    get_config("10.0.0.62", "9001", "admin", "passwd")
