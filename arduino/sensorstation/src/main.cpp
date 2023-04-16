5d417e16c40092abcaec6de29d287ffe855b6b6b
Frederik Hirsch <frederik.hirsch@student.uibk.ac.at>
2023-04-16 18:01:49 +0200

    added UUID from dip switch

--- arduino/sensorstation/src/main.cpp
+++ arduino/sensorstation/src/main.cpp
@@ -1,21 +1,37 @@
 #include <Arduino.h>
+#include <ArduinoBLE.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+
 Adafruit_BME680 bme;
+int dipSwitchPins[] = {D10, D9, D8, D7, D6, D5, D4, D3};
+
 
 void sensor_setup();
+
+void BLE_setup();
+
 unsigned int read_light_sensor();
+
 unsigned int read_hygrometer();
-void read_bme();
+
+void read_air_sensor();
+
+int get_UUID();
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
   while(!Serial);
+  Serial.println("Serial started");
+  int UUID = get_UUID();
+  Serial.println(UUID);
+  BLE_setup();
+  
   sensor_setup();
 }
 
@@ -43,6 +59,20 @@ void sensor_setup() {
   bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
+void BLE_setup() {
+  if (!BLE.begin()) {
+    Serial.println("Starting BLE failed!");
+
+    return;
+  }
+  Serial.println("BLE started");
+
+  BLE.setLocalName("SensorStation G2T4");
+  BLE.setDeviceName("SensorStation G2T4");
+
+  BLE.advertise();
+}
+
 unsigned int read_light_sensor() {
   unsigned int light = analogRead(A6);
   Serial.print("Light: ");
@@ -57,7 +87,7 @@ unsigned int read_hygrometer() {
   return audio;
 }
 
-void read_bme() {
+void read_air_sensor() {
   unsigned long endTime = bme.beginReading();
   if (endTime == 0) {
     Serial.println(F("Failed to begin reading :("));
@@ -94,4 +124,15 @@ void read_bme() {
   Serial.print(F("Approx. Altitude = "));
   Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
   Serial.println(F(" m"));
+}
+
+int get_UUID() {
+  for (int i = 0; i < 8; i++) {
+    pinMode(dipSwitchPins[i], INPUT_PULLDOWN);
+  }
+  int UUID = 0;
+  for (int i = 0; i < 8; i++) {
+    UUID += digitalRead(dipSwitchPins[i]) << i;
+  }
+  return UUID;
 }
