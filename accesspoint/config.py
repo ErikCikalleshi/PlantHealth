81630e9e9e820c169ba5455fcbaa1e7d409c8a6b
ErikCikalleshi <erik.cikalleshi@student.uibk.ac.at/home/ubuntu/g2t4/accesspoint/.venv/bin/python>
2023-04-19 08:05:45 +0000

    Fixed paths issue on raspberry pi

--- accesspoint/config.py
+++ accesspoint/config.py
@@ -4,17 +4,24 @@ import requests
 import os
 
 
-def read_settings():
-    with open("/home/ubuntu/g2t4/accesspoint/settings.json", "r") as data_file:
+def read_settings(file_path: str) -> dict:
+    if not os.path.exists(file_path):
+        os.makedirs(file_path, exist_ok=True)
+        file_path = os.path.join(file_path, "settings.json")
+        with open(file_path, "w") as f:
+            json.dump({}, f)
+    with open(file_path, "r") as data_file:
         data = json.load(data_file)
     return data
 
 
-INTERVAL: int = 10  # seconds
+INTERVAL: int = 1  # seconds
 
 
-def get_config():
-    settings: json = read_settings()
+def get_config(script_path):
+    current_dir = os.path.dirname(os.path.abspath(script_path))
+    sett = os.path.join(current_dir, "settings.json")
+    settings = read_settings(sett)
     admin = settings["auth"]["user"]
     password = settings["auth"]["password"]
     ip = settings["server"]["host"]
@@ -33,19 +40,22 @@ def get_config():
     # if response.status_code != 200:
     #     print("Error: 'api/setting/' API call failed")
     #     return response.status_code
-
-    current_dir = os.path.dirname(__file__)
-    print(current_dir)
-    filename = os.path.join(dir, "config")
-    print(filename)
     data = response.json()
-    with open(filename, "w+") as f:
+    current_dir = os.path.dirname(os.path.abspath(script_path))
+    config_dir_path = os.path.join(current_dir, "config")
+    config_file_path = os.path.join(config_dir_path, "config.json")
+
+    if not os.path.exists(config_dir_path):
+        os.makedirs(config_dir_path)
+
+    with open(config_file_path, "w+") as f:
         json.dump(data, f, indent=4)
-    threading.Timer(INTERVAL, get_config).start()
+    threading.Timer(INTERVAL, get_config, args=[script_path]).start()
     print("INFO: 'api/setting/' API call successful")
 
 
 # for debug purposes only
 if __name__ == "__main__":
     # NOTE: put your own IP address, port, admin and password
-    get_config()
+    script_path = os.path.abspath(__file__)
+    get_config(script_path)
