0f2ea9a0e8c8c2dc9286b9ef7ba5e092bbb0ae9a
pimpmfl <danielpopescu934@gmail.com>
2023-04-26 22:01:15 +0200

    feat(plants-view): added functional search bar

--- frontend/src/views/PlantsView.vue
+++ frontend/src/views/PlantsView.vue
@@ -5,21 +5,21 @@
             <div class="flex center justify-space-between mb-10">
                 <page-heading class="text-white">All Plants</page-heading>
                 <div class="w-[220px]">
-                    <!--                    <v-text-field-->
-                    <!--                        :loading="loading"-->
-                    <!--                        density="compact"-->
-                    <!--                        variant="solo"-->
-                    <!--                        label="Search plant"-->
-                    <!--                        append-inner-icon="mdi-magnify"-->
-                    <!--                        single-line-->
-                    <!--                        hide-details-->
-                    <!--                        v-model="searchValue"-->
-                    <!--                        @click:append-inner="loading = true;"-->
-                    <!--                    ></v-text-field>-->
+                    <v-text-field
+                        :loading="loading"
+                        density="compact"
+                        variant="solo"
+                        label="Search plant"
+                        append-inner-icon="mdi-magnify"
+                        single-line
+                        hide-details
+                        v-model="searchValue"
+                        @click:append-inner="loading = true;"
+                    ></v-text-field>
                 </div>
             </div>
             <div class="flex flex-wrap gap-3 ">
-                <div v-for="(greenhouse, index) in greenhouses" :key="index" class="w-[360px] bg-white h-[200px] flex shadow rounded-[5px]">
+                <div v-for="(greenhouse, index) in greenhousesList" :key="index" class="w-[360px] bg-white h-[200px] flex shadow rounded-[5px]">
                     <img src="@/assets/plant-pic-example.png" alt="not loaded" class="p-[14px]"/>
                     <div class="p-[15px] flex justify-space-between flex-col w-full">
                         <div>
@@ -47,7 +47,6 @@ import PageHeading from "@/components/general/PageHeading.vue";
 import mainContainer from "@/components/general/main_container.vue";
 import footerComponent from "@/components/general/footer.vue";
 import headerComponent from "@/components/general/header.vue";
-import user_card from "@/components/admin/user_card.vue";
 import axios from "axios";
 import {API_BASE_URL} from "@/services";
 interface GreenhouseData {
@@ -59,15 +58,28 @@ interface GreenhouseData {
 }
 export default defineComponent({
     name: "PlantsView",
-    components: {user_card, headerComponent, footerComponent, mainContainer, PageHeading, MainComponent, HeaderView},
+    components: {headerComponent, footerComponent, mainContainer, PageHeading, MainComponent, HeaderView},
     data() {
         return {
             greenhouses: [] as GreenhouseData[],
+            loading: false,
+            searchValue: ''
         }
     },
     methods: {
         navigateToGallery(id: number) {
             this.$router.push(`/gallery/${id}`)
+        }
+    },
+    computed: {
+        greenhousesList() {
+            if (this.searchValue.trim().length > 0) {
+                return this.greenhouses.filter((greenhouse) => {
+                    return greenhouse.name.toLowerCase().includes(this.searchValue.toLowerCase()) ||
+                        greenhouse.location.toLowerCase().includes(this.searchValue.toLowerCase())
+                })
+            }
+            return this.greenhouses;
         }
     },
     mounted() {
