6ffa9102290fbccf4346e61277bd1754cc39774b
doubleg <georg.gas@live.at>
2023-04-28 13:56:15 +0200

    created AuditLogController

--- backend/src/main/java/at/qe/backend/ui/controllers/admin/AuditLogController.java
+++ backend/src/main/java/at/qe/backend/ui/controllers/admin/AuditLogController.java
@@ -1,34 +1,29 @@
 package at.qe.backend.ui.controllers.admin;
 
 import at.qe.backend.models.AuditLog;
-import at.qe.backend.models.Userx;
 import at.qe.backend.services.AuditLogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/auditlogs")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
 public class AuditLogController {
-    private AuditLogService auditLogService;
 
     @Autowired
-    public AuditLogController(AuditLogService auditLogService) {
-        this.auditLogService = auditLogService;
-    }
+    private AuditLogService auditLogService;
 
-    @GetMapping("/")
+    @GetMapping("/admin/get-all-audit-logs")
     public List<AuditLog> getAllAuditLogs() {
         return auditLogService.getAllAuditLogs();
     }
 
-    @GetMapping("/admin/action/{action}")
+    @GetMapping("/admin/get-audit-logs-by-action/{action}")
     public List<AuditLog> getAuditLogsByAction(@PathVariable String action) {
         return auditLogService.getAuditLogsByAction(action);
     }
 }
+
 
