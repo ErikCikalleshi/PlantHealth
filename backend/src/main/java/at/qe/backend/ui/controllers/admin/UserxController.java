7aaecd8444e00bfe7564777b2b7102321acf7c69
doubleg <georg.gas@live.at>
2023-04-29 11:38:05 +0200

    added changes to audits in UserxService

--- backend/src/main/java/at/qe/backend/ui/controllers/admin/UserxController.java
+++ backend/src/main/java/at/qe/backend/ui/controllers/admin/UserxController.java
@@ -7,7 +7,6 @@ import at.qe.backend.exceptions.Userx.UserDoesNotExistException;
 import at.qe.backend.models.Userx;
 import at.qe.backend.models.dto.UserDTO;
 import at.qe.backend.models.request.CreateNewUserRequest;
-import at.qe.backend.services.AuditLogService;
 import at.qe.backend.services.UserxService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,8 +21,6 @@ import java.util.Collection;
 public class UserxController {
     @Autowired
     UserxService userxService;
-    @Autowired
-    AuditLogService auditLogService;
 
     /**
      * records are DTOs (Data Transfer Object) used to return only the fields we want back to the client
