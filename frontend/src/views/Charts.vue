8fdfd6f06bca8f271046b29d837137f36ccbe2f2
ErikCikalleshi <erik.cikalleshi@student.uibk.ac.at>
2023-04-29 19:04:39 +0200

    updated view for charts

--- frontend/src/views/Charts.vue
+++ frontend/src/views/Charts.vue
@@ -10,6 +10,8 @@ import PageHeading from "@/components/general/PageHeading.vue";
 import HeaderView from "@/components/general/header.vue";
 import FooterView from "@/components/general/footer.vue";
 import VueApexCharts from "vue3-apexcharts";
+import api from "@/services/api";
+import {th} from "vuetify/locale";
 
 export default defineComponent({
   name: "ChartsView",
@@ -22,9 +24,10 @@ export default defineComponent({
     footerComponent,
     mainContainer,
   },
-  data: function() {
+  data: function () {
     return {
-      reveal: false,
+      revealedCardIndex: -1,
+      expand: -1,
       chartOptions: {
         chart: {
           id: "vuechart-example",
@@ -39,98 +42,134 @@ export default defineComponent({
           data: [30, 40, 35, 50, 49, 60, 70, 91],
         },
       ],
+      sensorTypes: [] as string[],
     };
   },
   methods: {
+    getSensorType() {
+      let greenhouseUUID = this.$route.params.id;
+      console.log(this.$route.params.id)
 
+      api.get(`greenhouse/get/${greenhouseUUID}`).then((response) => {
+        response.data.sensors.forEach((sensor: { sensorType: string; }) => {
+          const words: any = sensor.sensorType.toLowerCase().split("_");
+          for (let i = 0; i < words.length; i++) {
+            words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+          }
+          this.sensorTypes.push(words.join(" "));
+        });
+      });
+    },
+    revealCard(index: number, scrollToChart: boolean) {
+      // Reveal or hide the card based on the index
+      this.expand = this.expand === index ? -1 : index;
+
+      if (scrollToChart) {
+        // Scroll to the chart if specified
+        this.$nextTick(() => {
+          this.scrollToChart();
+        });
+      }
+    },
+    scrollToChart() {
+      // Find the element for the revealed card
+      const cardElement = this.$refs.cardRef[this.expand];
+      if (cardElement) {
+        // Find the chart element within the card
+        const chartElement = cardElement.querySelector(".chart-container");
+        if (chartElement) {
+          // Scroll to the chart element
+          chartElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+      }
+    },
   },
   mounted() {
-
+    this.getSensorType();
   }
 
 
 });
 
 </script>
-
 <template>
   <v-app>
     <header-component />
     <main-container negative>
       <div class="container mx-auto">
-        <div class="bg-white rounded-lg p-6 shadow-lg">
-          <v-card
-              class="mx-auto"
-              max-width="344"
+        <v-row>
+          <v-col
+              v-for="(sensorType, index) in sensorTypes"
+              :key="sensorType"
+              :class="{ 'col-expanded': expand === index }"
+              cols="12"
           >
-            <v-card-text>
-              <div>Word of the Day</div>
-              <p class="text-h4 text--primary">
-                el·ee·mos·y·nar·y
-              </p>
-              <p>adjective</p>
-              <div class="text--primary">
-                relating to or dependent on charity; charitable.<br>
-                "an eleemosynary educational institution."
-              </div>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
-                  variant="text"
-                  color="teal-accent-4"
-                  @click="reveal = true"
-              >
-                Learn More
-              </v-btn>
-            </v-card-actions>
+            <v-card ref="cardRef" class="card-item">
+              <v-card-item :title="sensorType">
+                <template v-slot:subtitle>
+                  <v-icon
+                      icon="mdi-alert"
+                      size="18"
+                      color="error"
+                      class="me-1 pb-1"
+                  ></v-icon>
+                  Extreme Weather Alert
+                </template>
+              </v-card-item>
+              <v-card-text class="py-0">
+                <v-row align="center" no-gutters>
+                  <v-col class="text-h2" cols="6">
+                    64&deg;F
+                  </v-col>
 
-            <v-expand-transition>
-              <v-card
-                  v-if="reveal"
-                  class="v-card--reveal"
-                  style="height: 100%;"
-              >
-                <v-card-text class="pb-0">
-                  <p class="text-h4 text--primary">
-                    Origin
-                  </p>
-                  <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
-                </v-card-text>
-                <v-card-actions class="pt-0">
-                  <v-btn
-                      variant="text"
-                      color="teal-accent-4"
-                      @click="reveal = false"
-                  >
-                    Close
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-expand-transition>
-          </v-card>
-          <apexchart
-              width="500"
-              type="bar"
-              :options="chartOptions"
-              :series="series"
-          ></apexchart>
-        </div>
+                  <v-col cols="6" class="text-right">
+                    <v-icon
+                        color="error"
+                        icon="mdi-weather-hurricane"
+                        size="88"
+                    ></v-icon>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+
+              <div class="d-flex py-3 justify-space-between">
+                <v-list-item density="compact" prepend-icon="mdi-weather-windy">
+                  <v-list-item-subtitle>123 km/h</v-list-item-subtitle>
+                </v-list-item>
+
+                <v-list-item density="compact" prepend-icon="mdi-weather-pouring">
+                  <v-list-item-subtitle>48%</v-list-item-subtitle>
+                </v-list-item>
+              </div>
+              <v-expand-transition>
+                <template v-if="expand === index">
+
+                  <apexchart width="100%" type="bar" :options="chartOptions" :series="series" />
+                </template>
+              </v-expand-transition>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-btn @click="revealCard(index, true)">
+                  {{ expand === index ? "Hide Report" : "Full Report" }}
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
       </div>
     </main-container>
     <footer-component />
   </v-app>
 </template>
 
-
 <style>
-
-.v-card--reveal {
-  bottom: 0;
-  opacity: 1 !important;
-  position: absolute;
-  width: 100%;
+.col-expanded {
+  flex-grow: 1;
 }
 
-
-
+.card-item {
+  margin-bottom: 10px;
+}
 </style>
