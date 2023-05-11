c35a3b1b8f5fc64b8e1de13bc6121f6a79d1abb4
Tobias Beiser <tobias.beiser@gmail.com>
2023-05-11 20:59:17 +0200

    Updated landing page and header links

--- frontend/src/views/Landingpage.vue
+++ frontend/src/views/Landingpage.vue
@@ -1,31 +1,37 @@
 <template>
     <v-app>
         <header-view presentation>
-            <div class="container mx-auto">
-                <v-carousel height="64vh"
-                            theme="light"
-                            cycle
-                            hide-delimiter-background
-                >
-                    <v-carousel-item>
+            <div class="container mx-auto h-[64vh]">
+<!--                <v-carousel height="64vh"-->
+<!--                            theme="light"-->
+<!--                            cycle-->
+<!--                            hide-delimiter-background-->
+<!--                >-->
+<!--                    <v-carousel-item>-->
                         <div class="carousel__item">
-                            <h2 class="text-[44px] md:text-[60px]">Start tracking your plants with <br><span class="text-[54px] md:text-[80px]">Plant Health</span></h2>
-                            <router-link class="dashboard-button" to="/dashboard" tag="button">View Dashboard</router-link>
+                            <h2 class="text-[44px] md:text-[60px]">Start tracking your plants with <br><span
+                                    class="text-[54px] md:text-[80px]">Plant Health</span></h2>
+                            <router-link class="dashboard-button" to="/dashboard" tag="button">View Dashboard
+                            </router-link>
                         </div>
-                    </v-carousel-item>
-                    <v-carousel-item>
-                        <div class="carousel__item">
-                            <h2 class="text-[44px] md:text-[60px]">Start tracking your plants with <br><span class="text-[54px] md:text-[80px]">Plant Health</span></h2>
-                            <router-link class="dashboard-button" to="/dashboard" tag="button">View Dashboard</router-link>
-                        </div>
-                    </v-carousel-item>
-                    <v-carousel-item>
-                        <div class="carousel__item">
-                            <h2 class="text-[44px] md:text-[60px]">Start tracking your plants with <br><span class="text-[54px] md:text-[80px]">Plant Health</span></h2>
-                            <router-link class="dashboard-button" to="/dashboard" tag="button">View Dashboard</router-link>
-                        </div>
-                    </v-carousel-item>
-                </v-carousel>
+<!--                    </v-carousel-item>-->
+<!--                    <v-carousel-item>-->
+<!--                        <div class="carousel__item">-->
+<!--                            <h2 class="text-[44px] md:text-[60px]">Start tracking your plants with <br><span-->
+<!--                                    class="text-[54px] md:text-[80px]">Plant Health</span></h2>-->
+<!--                            <router-link class="dashboard-button" to="/dashboard" tag="button">View Dashboard-->
+<!--                            </router-link>-->
+<!--                        </div>-->
+<!--                    </v-carousel-item>-->
+<!--                    <v-carousel-item>-->
+<!--                        <div class="carousel__item">-->
+<!--                            <h2 class="text-[44px] md:text-[60px]">Start tracking your plants with <br><span-->
+<!--                                    class="text-[54px] md:text-[80px]">Plant Health</span></h2>-->
+<!--                            <router-link class="dashboard-button" to="/dashboard" tag="button">View Dashboard-->
+<!--                            </router-link>-->
+<!--                        </div>-->
+<!--                    </v-carousel-item>-->
+<!--                </v-carousel>-->
             </div>
         </header-view>
         <div class="container mx-auto">
@@ -34,14 +40,37 @@
                     <div class="flex gap-2">
                         <img alt="leaf" src="../assets/leaf.svg"/>
                         <div>
-                            <h2 class="font-primary text-primary text-uppercase text-[24px] leading-[23.28px]">About Plant Health</h2>
-                            <p class="font-secondary text-[16px] leading-[15.52px] mt-[4px]">Everything you need to know about Plant Health.</p>
+                            <h2 class="font-primary text-primary text-uppercase text-[24px] leading-[23.28px]">About
+                                Plant Health</h2>
+                            <p class="font-secondary text-[16px] leading-[15.52px] mt-[4px]">Everything you need to know
+                                about Plant Health.</p>
                         </div>
                     </div>
                     <p class="font-secondary text-[20px] leading-[138%] pt-[50px]">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere urna ac ipsum blandit maximus. Nunc iaculis, ipsum eget dignissim sagittis, nibh nisi vulputate ex, vehicula aliquam risus leo eu sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ut sollicitudin ipsum. Integer in egestas libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi tristique felis eu ullamcorper dictum. Nullam ex dolor, elementum ac semper sed, vestibulum v itae mauris
+                        At PlantHealth, we believe that keeping your plants healthy and thriving should be simple and
+                        hassle-free. Our innovative service makes it easy to monitor the vital data of various plants in
+                        mini-greenhouses using state-of-the-art technology. <br>
+                        Each plant is equipped with an Arduino controller that collects and transmits data to a
+                        Raspberry Pi access point via Bluetooth Low Energy (BLE) technology. With multiple access points
+                        available, each with many plants, our system can keep track of all plants in a company,
+                        providing real-time data about their growth and health.<br>
+                        Our service is designed with simplicity and efficiency in mind. Only admins and gardeners have
+                        the ability to manage sensor limits, ensuring that each plant receives the optimal conditions
+                        for its growth. Additionally, only admins can manage the transmission interval, giving them
+                        complete control over how often data is transmitted to the web server.
+
+<!--                        On the web server, gardeners can see the change in the sensor values via charts, allowing them-->
+<!--                        to detect any issues and take necessary action to maintain the health of the plant.-->
+<!--                        Non-registered users can upload images of the greenhouse or plant via the QR code on the-->
+<!--                        mini-greenhouse. Registered users can view all plants in the system and browse through the-->
+<!--                        uploaded pictures, providing an easy way to monitor the progress of each plant.-->
+
+<!--                        Our goal at PlantHealth is to provide a user-friendly, efficient, and effective service that-->
+<!--                        helps companies take care of their plants in a simple and hassle-free way. Contact us today to-->
+<!--                        learn more about how our service can benefit your company's plant management needs.-->
                     </p>
-                    <a class="text-primary font-primary text-[20px] underline hover:no-underline mt-[20px]" href="#">View More</a>
+                    <router-link class="text-primary font-primary text-[20px] underline hover:no-underline mt-[20px]" to="about">View
+                        More</router-link>
                 </div>
                 <img alt="projectPic" src="@/assets/projectExample.png"/>
             </div>
@@ -50,8 +79,10 @@
                     <div class="flex gap-4">
                         <img alt="leaf" src="@/assets/leaf.svg"/>
                         <div>
-                            <h2 class="text-uppercase font-primary text-primary text-[24px] leading-[23.28px] pb-[4px]">Contact</h2>
-                            <p class="font-secondary text-gray-500 text-[16px] leading-[15.52px] mt-[4px]">If you have any enquiries, fell free to contact us.</p>
+                            <h2 class="text-uppercase font-primary text-primary text-[24px] leading-[23.28px] pb-[4px]">
+                                Contact</h2>
+                            <p class="font-secondary text-gray-500 text-[16px] leading-[15.52px] mt-[4px]">If you have
+                                any enquiries, fell free to contact us.</p>
                         </div>
                     </div>
                     <div class="grid grid-cols-1 xl:grid-cols-2 pt-[60px] gap-[100px]">
@@ -110,9 +141,12 @@
                             <p class="text-white font-secondary">— Thursday: 08:00 - 17:00</p>
                             <p class="text-white font-secondary">— Friday: 08:00 - 17:00</p>
                             <div class="pt-[70px]">
-                                <p class="font-secondary text-white">Phone Number: <span class="font-primary text-white">+43 000 000 000</span></p>
-                                <p class="font-secondary text-white">Customer Services: <span class="font-primary text-white">customerservices@plant-health.at</span></p>
-                                <p class="font-secondary text-white">Sales: <span class="font-primary text-white">sales@plant-health.at</span></p>
+                                <p class="font-secondary text-white">Phone Number: <span
+                                        class="font-primary text-white">+43 000 000 000</span></p>
+                                <p class="font-secondary text-white">Customer Services: <span
+                                        class="font-primary text-white">customerservices@plant-health.at</span></p>
+                                <p class="font-secondary text-white">Sales: <span class="font-primary text-white">sales@plant-health.at</span>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -127,8 +161,21 @@
 import {useStore} from '@/stores/user/user';
 import HeaderView from "@/components/general/header.vue";
 import FooterView from "@/components/general/footer.vue";
+import {router} from "@/router";
+import about from "@/views/About.vue";
+
 export default {
     name: 'LandingPage',
+    computed: {
+        about() {
+            return about
+        }
+    },
+    methods: {
+        router() {
+            return router
+        }
+    },
     components: {FooterView, HeaderView},
     data() {
         return {
@@ -151,18 +198,22 @@ export default {
     justify-content: center;
     align-items: center;
 }
+
 .carousel__item .dashboard-button {
     padding: 18px 35px;
     font-size: 20px;
     background: white;
     outline: none;
 }
+
 .v-carousel__controls {
     background: transparent;
 }
+
 .v-carousel__controls .v-btn__content {
     color: #ffffff;
 }
+
 .carousel__item h2 {
     letter-spacing: -0.05em;
     text-transform: uppercase;
@@ -172,6 +223,7 @@ export default {
     text-align: center;
     width: 80%;
 }
+
 .carousel__item span {
     line-height: 92.5%;
 }
