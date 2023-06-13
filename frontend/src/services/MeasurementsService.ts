f7570b0faed90846d9eef64bb3876ca65bc0979b
Tobias Beiser <tobias.beiser@gmail.com>
2023-06-13 14:31:05 +0200

    updated charts to actually represent the past limits. breaking change: need to update how accesspoint sends data to webserver

--- frontend/src/services/MeasurementsService.ts
+++ frontend/src/services/MeasurementsService.ts
@@ -5,7 +5,9 @@ class MeasurementsService {
         return await api.get('charts/get-measurements-by-greenhouse-id/' + greenhouseId, {});
     }
     async getMeasurementsByGreenhouseIdAndSensorType(greenhouseId: number, sensorType: string) {
-        return await api.get('charts/get-measurements-by-greenhouse-id-and-sensor-type/' + greenhouseId + '/' + sensorType, {});
+        const response=await api.get('charts/get-measurements-by-greenhouse-id-and-sensor-type/' + greenhouseId + '/' + sensorType, {});
+        console.log(response)
+        return response
     }
 
 
