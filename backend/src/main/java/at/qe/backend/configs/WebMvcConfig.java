a6db935130522af128902444c41e1c7993151000
Tobias Beiser <tobias.beiser@gmail.com>
2023-06-18 16:36:27 +0200

    fixed datetime display

--- backend/src/main/java/at/qe/backend/configs/WebMvcConfig.java
+++ backend/src/main/java/at/qe/backend/configs/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOriginPatterns("http://127.0.0.1:5173", "http://localhost:5173", "http://172.30.208.1:5173")
+                .allowedOriginPatterns("http://127.0.0.1:5173", "http://localhost:5173", "http://172.20.10.2:5173")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Set the preflight request cache duration
